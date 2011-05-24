00f7733158383950c5b446edfb0ff429f64b5f6a
Nicolas Noirbent <nicolas.noirbent@smartjog.com>
2011-05-24 16:43:02 +0200

    Add a static file status plugin

--- savate/status.py
+++ savate/status.py
@@ -52,3 +52,24 @@ class JSONStatusClient(BaseStatusClient):
         return HTTPEventHandler(self.server, sock, address, request_parser,
                                 200, b'OK', {b'Content-Type': 'application/json'},
                                 json.dumps(status_dict, indent = 4) + '\n')
+
+
+class StaticFileStatusClient(BaseStatusClient):
+
+    def __init__(self, server, server_config, **config_dict):
+        BaseStatusClient.__init__(self, server, server_config, **config_dict)
+        self.static_filename = config_dict['static_file']
+
+    def get_status(self, sock, address, request_parser):
+        try:
+            with open(self.static_filename) as static_fileobj:
+                status_body = static_fileobj.read()
+            return HTTPEventHandler(self.server, sock, address, request_parser,
+                                    200, b'OK', {b'Content-Type': 'application/octet-stream'},
+                                    status_body)
+        except IOError, exc:
+            self.server.logger.exception('Error when trying to serve static status file %s:',
+                                         self.static_filename)
+            return HTTPEventHandler(self.server, sock, address, request_parser,
+                                    500, b'Internal Server Error', {b'Content-Type': 'text/plain'},
+                                    'Failed to open static status file\n')
