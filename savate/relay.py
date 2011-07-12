noirbee/savate

commit 8d018bca9e42099ea3619a8e4971a9b345fc2bf5

Minor coding style fix

savate/relay.py
@@ -72,7 +72,7 @@ class UDPRelay(Relay):
                     # EAGAIN, we'll come back later
                     break
                 else:
-                    self.initial_buffer_data += tmp_buffer
+                    self.initial_buffer_data += self.initial_buffer_data + tmp_buffer
                 if len(self.initial_buffer_data) >= self.MIN_START_BUFFER:
                     # OK, this looks like a valid source (since there
                     # is some socket activity)

commit ec5618530bb5be19d3dc0b892527c1f2ddfddcd7

Fix data buffering bug introduced in 8d018bca

savate/relay.py
@@ -72,7 +72,7 @@ class UDPRelay(Relay):
                     # EAGAIN, we'll come back later
                     break
                 else:
-                    self.initial_buffer_data += self.initial_buffer_data + tmp_buffer
+                    self.initial_buffer_data = self.initial_buffer_data + tmp_buffer
                 if len(self.initial_buffer_data) >= self.MIN_START_BUFFER:
                     # OK, this looks like a valid source (since there
                     # is some socket activity)