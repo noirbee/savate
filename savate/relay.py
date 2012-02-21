noirbee/savate

commit d40916bec3bbdb57cfd56c93377db7bbccc06873

Unknown Content-Type streams are now accepted and handled in a generic way

savate/relay.py
@@ -183,25 +183,24 @@ class HTTPRelay(Relay):
                                      (self.sock, self.address))
             self.close()
             return
-        content_type = self.response_parser.headers.get('Content-Type',
-                                                        'application/octet-stream')
+
         # FIXME: similar code is present in server.py
         loop = self.server.loop
+        self.server.logger.info('New source for %s: %s', self.path, self.address)
+
+        content_type = self.response_parser.headers.get('Content-Type',
+                                                        'application/octet-stream')
         if content_type in sources.sources_mapping:
-            self.server.logger.info('New source for %s: %s', self.path, self.address)
-            source = sources.sources_mapping[content_type](self.server,
-                                                           self.sock,
-                                                           self.address,
-                                                           content_type,
-                                                           self.response_parser,
-                                                           self.path)
-            self.server.add_source(self.path, source)
+            source_class = sources.sources_mapping[content_type]
         else:
-            self.server.logger.warning('Unrecognized Content-Type %s', content_type)
-            loop.register(helpers.HTTPEventHandler(self.server,
-                                                   self.sock,
-                                                   self.address,
-                                                   self.response_parser,
-                                                   501,
-                                                   b'Not Implemented'),
-                          looping.POLLOUT)
+            self.server.logger.warning('No registered source handler for %s, using generic handler',
+                                       content_type)
+            source_class = sources.BufferedRawSource
+
+        source = source_class(self.server,
+                              self.sock,
+                              self.address,
+                              content_type,
+                              self.response_parser,
+                              self.path)
+        self.server.add_source(path, source)

configure.ac
@@ -2,7 +2,7 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ([2.67])
-AC_INIT([savate], [0.4.1~dev], [nicolas@noirbent.fr])
+AC_INIT([savate], [0.5.0~dev], [nicolas@noirbent.fr])
 
 AC_CONFIG_HEADERS([config.h])
 

savate/server.py
@@ -115,19 +115,24 @@ class HTTPRequest(looping.BaseIOEventHandler):
 
         if self.request_parser.request_method in [b'PUT', b'SOURCE', b'POST']:
             # New source
+            self.server.logger.info('New source for %s: %s', path, self.address)
+
             content_type = self.request_parser.headers.get('Content-Type',
                                                            'application/octet-stream')
             if content_type in sources.sources_mapping:
-                self.server.logger.info('New source for %s: %s', path, self.address)
-                source = sources.sources_mapping[content_type](self.server,
-                                                               self.sock,
-                                                               self.address,
-                                                               content_type,
-                                                               self.request_parser)
-                self.server.add_source(path, source)
+                source_class = sources.sources_mapping[content_type]
             else:
-                self.server.logger.warning('Unrecognized Content-Type %s', content_type)
-                response = HTTPResponse(501, b'Not Implemented')
+                self.server.logger.warning('No registered source handler for %s, using generic handler',
+                                           content_type)
+                source_class = sources.BufferedRawSource
+
+            source = source_class(self.server,
+                                  self.sock,
+                                  self.address,
+                                  content_type,
+                                  self.request_parser)
+            self.server.add_source(path, source)
+
         elif self.request_parser.request_method in [b'GET']:
             # New client
 

commit 13c2cb684c822d65e35b9e4e5b4ce8a9c17f679f

Fix HTTP relaying
  * Which I broke in d40916be.

savate/relay.py
@@ -203,4 +203,4 @@ class HTTPRelay(Relay):
                               content_type,
                               self.response_parser,
                               self.path)
-        self.server.add_source(path, source)
+        self.server.add_source(self.path, source)