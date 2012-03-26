noirbee/savate

commit 3b4f1e3a1195347781a05fa0ae88785bfafe95e0

Refactored source selection.
Removed duplicated code and added a function which returns a StreamSource
instance depending on context (previously only Content-Type).

savate/server.py
@@ -114,25 +114,7 @@ class HTTPRequest(looping.BaseIOEventHandler):
         response = None
 
         if self.request_parser.request_method in [b'PUT', b'SOURCE', b'POST']:
-            # New source
-            self.server.logger.info('New source for %s: %s', path, self.address)
-
-            content_type = self.request_parser.headers.get('Content-Type',
-                                                           'application/octet-stream')
-            if content_type in sources.sources_mapping:
-                source_class = sources.sources_mapping[content_type]
-            else:
-                self.server.logger.warning('No registered source handler for %s, using generic handler',
-                                           content_type)
-                source_class = sources.BufferedRawSource
-
-            source = source_class(self.server,
-                                  self.sock,
-                                  self.address,
-                                  content_type,
-                                  self.request_parser)
-            self.server.add_source(path, source)
-
+            self.server.add_source(path, self.sock, self.address, self.request_parser)
         elif self.request_parser.request_method in [b'GET']:
             # New client
 
@@ -294,7 +276,14 @@ class TCPServer(looping.BaseIOEventHandler):
     def add_status_handler(self, path, handler):
         self.status_handlers[path] = handler
 
-    def add_source(self, path, source):
+    def add_source(self, path, sock, address, request_parser,
+                   burst_size = None):
+        # New source
+        self.logger.info('New source for %s: %s', path, self.address)
+
+        source = sources.find_source(self, sock, address, request_parser, path,
+                                     burst_size)
+
         self.sources.setdefault(path, {})[source] = {'source': source,
                                                      'clients': {}}
         self.timeouts.update_timeout(source, int(self.loop.now()) + self.INACTIVITY_TIMEOUT)

savate/relay.py
@@ -188,24 +188,5 @@ class HTTPRelay(Relay):
             self.close()
             return
 
-        # FIXME: similar code is present in server.py
-        loop = self.server.loop
-        self.server.logger.info('New source for %s: %s', self.path, self.address)
-
-        content_type = self.response_parser.headers.get('Content-Type',
-                                                        'application/octet-stream')
-        if content_type in sources.sources_mapping:
-            source_class = sources.sources_mapping[content_type]
-        else:
-            self.server.logger.warning('No registered source handler for %s, using generic handler',
-                                       content_type)
-            source_class = sources.BufferedRawSource
-
-        source = source_class(self.server,
-                              self.sock,
-                              self.address,
-                              content_type,
-                              self.response_parser,
-                              self.path,
-                              self.burst_size)
-        self.server.add_source(self.path, source)
+        self.server.add_source(self.path, self.sock, self.address,
+                               self.response_parser, self.burst_size)

savate/sources.py
@@ -201,3 +201,22 @@ sources_mapping = {
     b'application/octet-stream': BufferedRawSource,
     b'video/MP2T': MPEGTSSource,
     }
+
+
+def find_source(server, sock, address, request_parser,
+                path = None, burst_size = None):
+    """Return a :class:`StreamSource` instance."""
+
+    content_type = request_parser.headers.get('Content-Type',
+                                      'application/octet-stream')
+    if content_type in sources_mapping:
+        stream_source = sources_mapping[content_type]
+    else:
+        server.logger.warning(
+            'No registered source handler for %s, using generic handler',
+            content_type,
+        )
+        stream_source = BufferedRawSource
+
+    return stream_source(server, sock, address, content_type, request_parser,
+                         path, burst_size)

commit 22a0e0c461d55160fe843d01ed157fcda758e3fd

Fixed log message when new source is added to the server.
Was broken by 3b4f1e3a1195347781a05fa0ae88785bfafe95e0.

savate/server.py
@@ -278,7 +278,7 @@ class TCPServer(looping.BaseIOEventHandler):
     def add_source(self, path, sock, address, request_parser,
                    burst_size = None):
         # New source
-        self.logger.info('New source for %s: %s', path, self.address)
+        self.logger.info('New source for %s: %s', path, address)
 
         source = sources.find_source(self, sock, address, request_parser, path,
                                      burst_size)