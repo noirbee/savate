noirbee/savate

commit eb739984e40769a2e40bc7c65fec51dd5446ae37

Implemented on demand feature.
Go get the sources (in relay mode) at startup then disconnect.
Reconnect when clients connect and ask for the source.
Disconnect when there is no more listeners after a timeout.
Setting can be done globally or by mount point (or both).
On demand is disabled by default if not present in configuration.

savate/server.py
@@ -114,7 +114,8 @@ class HTTPRequest(looping.BaseIOEventHandler):
         response = None
 
         if self.request_parser.request_method in [b'PUT', b'SOURCE', b'POST']:
-            self.server.add_source(path, self.sock, self.address, self.request_parser)
+            self.server.register_source(sources.find_source(
+                path, self.sock, self.address, self.request_parser))
         elif self.request_parser.request_method in [b'GET']:
             # New client
 
@@ -272,13 +273,14 @@ class TCPServer(looping.BaseIOEventHandler):
     def configure(self):
         self.config.configure()
 
-    def add_relay(self, url, path, address_info = None, burst_size = None):
+    def add_relay(self, url, path, address_info = None, burst_size = None,
+                  on_demand = False):
         if urlparse.urlparse(url).scheme in ('udp', 'multicast'):
             tmp_relay = relay.UDPRelay(self, url, path, address_info,
                                        burst_size)
         else:
             tmp_relay = relay.HTTPRelay(self, url, path, address_info,
-                                        burst_size)
+                                        burst_size, on_demand)
         self.relays[tmp_relay.sock] = tmp_relay
 
     def add_auth_handler(self, handler):
@@ -289,12 +291,14 @@ class TCPServer(looping.BaseIOEventHandler):
 
     def add_source(self, path, sock, address, request_parser,
                    burst_size = None):
-        # New source
-        self.logger.info('New source for %s: %s', path, address)
 
         source = sources.find_source(self, sock, address, request_parser, path,
                                      burst_size)
+        self.register_source(source)
 
+    def register_source(self, source):
+        self.logger.info('New source (%s) for %s: %s',
+                         source.__class__.__name__, source.path, source.address)
         self.sources.setdefault(path, {})[source] = {'source': source,
                                                      'clients': {}}
         self.reset_inactivity_timeout(source)
@@ -314,6 +318,8 @@ class TCPServer(looping.BaseIOEventHandler):
     def remove_source(self, source):
         # De-activate the timeout handling for this source
         self.remove_inactivity_timeout(source)
+        # Remove on demand closing timeout
+        self.timeouts.remove_timeout(source)
         # FIXME: client shutdown
         if len(self.sources[source.path]) > 1:
             # There is at least one other source for this path,
@@ -324,6 +330,8 @@ class TCPServer(looping.BaseIOEventHandler):
                                                      itertools.cycle(self.sources[source.path].keys())):
                 client.source = new_source
                 self.sources[source.path][new_source]['clients'][client.fileno()] = client
+                # if source is on demand and not running, then start it
+                new_source.on_demand_activate()
         else:
             for client in self.sources[source.path][source]['clients'].values():
                 client.close()
@@ -363,7 +371,8 @@ class TCPServer(looping.BaseIOEventHandler):
                 self.logger.info('Restarting relay %s', self.relays_to_restart[0][1])
                 tmp_relay = self.relays_to_restart.popleft()[1]
                 self.add_relay(tmp_relay.url, tmp_relay.path,
-                               tmp_relay.addr_info, tmp_relay.burst_size)
+                               tmp_relay.addr_info, tmp_relay.burst_size,
+                               tmp_relay.on_demand)
 
             if self.reloading:
                 self.reloading = False

savate/sources.py
@@ -8,9 +8,13 @@ class StreamSource(looping.BaseIOEventHandler):
 
     # Incoming maximum buffer size
     RECV_BUFFER_SIZE = 64 * 2**10
+    # Stay connected for 20 seconds to the source when all clients are
+    # disconnected
+    ON_DEMAND_TIMEOUT = 20
 
     def __init__(self, server, sock, address, content_type,
-                 request_parser = None, path = None, burst_size = None):
+                 request_parser = None, path = None, burst_size = None,
+                 on_demand=False):
         self.server = server
         self.sock = sock
         self.address = address
@@ -18,6 +22,51 @@ class StreamSource(looping.BaseIOEventHandler):
         self.request_parser = request_parser
         self.path = path or self.request_parser.request_path
         self.burst_size = burst_size
+
+        # states
+        # 0: no on demand
+        # 1: on demand not running
+        # 2: on demand connecting
+        # 3: on demand running
+        # 4: on demand running but about to close
+        self.on_demand = 3 if on_demand else 0
+        self.relay = server.relays[sock]
+
+    def on_demand_activate(self):
+        """Method which reconnects the relay"""
+        # activate only if state 1
+        if self.on_demand == 4:
+            # cancel on-demand closing
+            self.server.timeouts.remove_timeout(self)
+            return
+        elif self.on_demand != 1:
+            return
+
+        self.server.logger.info('Activate ondemand for source %s: %s',
+                                self.path, self.address)
+        self.on_demand = 2
+        del self.server.relays[self.sock]
+        self.relay.connect()
+        self.server.relays[self.relay.sock] = self.relay
+
+    def on_demand_desactivate(self):
+        """Cleanup when on_demand is desactivated. It can be overided in
+        subclasses to clear buffers for examples.
+
+        """
+        self.server.logger.info('Desactivate ondemand for source %s: %s',
+                                self.path, self.address)
+        self.on_demand = 1
+        self.server.loop.unregister(self)
+        self.server.remove_inactivity_timeout(self)
+        self.sock.close()
+
+    def on_demand_connected(self, sock, request_parser):
+        """Method called by the relay when it did reconnect sucessfully."""
+        self.on_demand = 3
+        self.sock = sock
+        self.request_parser = request_parser
+        self.server.loop.register(self, looping.POLLIN)
 
     def __str__(self):
         return '<%s for %s, %s, %s>' % (
@@ -29,6 +78,7 @@ class StreamSource(looping.BaseIOEventHandler):
 
     def close(self):
         self.server.remove_source(self)
+        self.relay = None  # prevent cyclic reference
         looping.BaseIOEventHandler.close(self)
 
     def recv_packet(self, buffer_size = RECV_BUFFER_SIZE):
@@ -62,11 +112,25 @@ class StreamSource(looping.BaseIOEventHandler):
         self.publish_packet(packet)
 
     def publish_packet(self, packet):
+        clients = self.server.sources[self.path][self]['clients']
+
+        if not clients and self.on_demand == 3:
+            # activate timeout for desactivating source
+            self.on_demand = 4
+            self.server.timeouts.reset_timeout(
+                self,
+                self.server.loop.now() + self.ON_DEMAND_TIMEOUT,
+                self.on_demand_desactivate,
+            )
+
         self.server.publish_packet(self, packet)
 
     def new_client(self, client):
-        # Do nothing by default
-        pass
+        if self.on_demand == 1:
+            self.on_demand_activate()
+        elif self.on_demand == 4:
+            self.on_demand = 3
+            self.server.timeouts.remove_timeout(self)
 
     def update_burst_size(self, new_burst_size):
         pass
@@ -81,9 +145,10 @@ class BufferedRawSource(StreamSource):
     BURST_SIZE = 64 * 2**10
 
     def __init__(self, server, sock, address, content_type,
-                 request_parser = None , path = None, burst_size = None):
+                 request_parser = None , path = None, burst_size = None,
+                 on_demand=False):
         StreamSource.__init__(self, server, sock, address, content_type,
-                              request_parser, path, burst_size)
+                              request_parser, path, burst_size, on_demand)
         if request_parser:
             self.output_buffer_data = request_parser.body
         else:
@@ -99,7 +164,17 @@ class BufferedRawSource(StreamSource):
             self.burst_packets.append(self.output_buffer_data)
             self.output_buffer_data = ''
 
+    def on_demand_desactivate(self):
+        self.output_buffer_data = b''
+        self.burst_packets.clear()
+        StreamSource.on_demand_desactivate(self)
+
+    def on_demand_connected(self, sock, request_parser):
+        StreamSource.on_demand_connected(self, sock, request_parser)
+        self.output_buffer_data = request_parser.body
+
     def new_client(self, client):
+        StreamSource.new_client(self, client)
         for packet in self.burst_packets:
             client.add_packet(packet)
 
@@ -207,7 +282,7 @@ sources_mapping = {
 
 
 def find_source(server, sock, address, request_parser,
-                path = None, burst_size = None):
+                path = None, burst_size = None, on_demand = False):
     """Return a :class:`StreamSource` instance."""
 
     content_type = request_parser.headers.get('Content-Type',
@@ -222,4 +297,4 @@ def find_source(server, sock, address, request_parser,
         stream_source = BufferedRawSource
 
     return stream_source(server, sock, address, content_type, request_parser,
-                         path, burst_size)
+                         path, burst_size, on_demand)

savate/configuration.py
@@ -116,6 +116,7 @@ class ServerConfiguration(object):
         conf = self.config_dict
         server = self.server
         global_burst_size = conf.get('burst_size', None)
+        global_on_demand = conf.get('on_demand', False)
 
         net_resolve_all = conf.get('net_resolve_all', False)
 
@@ -135,6 +136,7 @@ class ServerConfiguration(object):
 
             mount_burst_size = convert_burst_size(
                 mount_conf.get('burst_size', global_burst_size))
+            mount_on_demand = mount_conf.get('on_demand', global_on_demand)
             path = mount_conf['path']
             for source_url in mount_conf['source_urls']:
                 parsed_url = urlparse.urlparse(source_url)
@@ -156,12 +158,14 @@ class ServerConfiguration(object):
                                 server.logger.info('Trying to relay %s from %s:%s', source_url,
                                             address_info[4][0], address_info[4][1])
                                 server.add_relay(source_url, path, address_info,
-                                                 mount_burst_size)
+                                                 mount_burst_size,
+                                                 mount_on_demand)
                     else:
                         if (source_url, path) not in relay_index:
                             server.logger.info('Trying to relay %s', source_url)
                             server.add_relay(source_url, path,
-                                             burst_size=mount_burst_size)
+                                             burst_size=mount_burst_size,
+                                             on_demand=mount_on_demand)
 
     def configure_authorization(self):
         conf = self.config_dict

savate/flv_source.py
@@ -11,9 +11,9 @@ class FLVSource(StreamSource):
     BURST_DURATION = 5 * 1000
 
     def __init__(self, sock, server, address, content_type, request_parser,
-                 path = None, burst_size = None):
+                 path = None, burst_size = None, on_demand=False):
         StreamSource.__init__(self, sock, server, address, content_type,
-                              request_parser, path, burst_size)
+                              request_parser, path, burst_size, on_demand)
         # Initial buffer data
         self.buffer_data = request_parser.body
         # The FLV stream header
@@ -32,7 +32,23 @@ class FLVSource(StreamSource):
         # At startup we want to parse the stream header
         self.handle_data = self.handle_header
 
+    def on_demand_desactivate(self):
+        StreamSource.on_demand_desactivate(self)
+        self.stream_header = None
+        self.got_initial_meta = self.got_initial_audio = self.got_initial_video = False
+        self.initial_tags.clear()
+        self.packets_group.clear()
+        self.burst_groups.clear()
+        self.burst_groups_data.clear()
+        self.handle_data = self.handle_header
+        self.buffer_data = b''
+
+    def on_demand_connected(self, sock, request_parser):
+        self.buffer_data = request_parser.body
+        StreamSource.on_demand_connected(self, sock, request_parser)
+
     def new_client(self, client):
+        StreamSource.new_client(self, client)
         if self.stream_header:
             client.add_packet(self.stream_header.raw_data)
         for tag in self.initial_tags:

savate/relay.py
@@ -94,9 +94,16 @@ class HTTPRelay(Relay):
     HTTP_VERSION = b'HTTP/1.1'
     RESPONSE_MAX_SIZE = 4096
 
-    def __init__(self, server, url, path, addr_info = None, burst_size = None):
+    def __init__(self, server, url, path, addr_info = None, burst_size = None,
+                 on_demand=False):
         Relay.__init__(self, server, url, path, addr_info, burst_size)
 
+        self.on_demand = bool(on_demand)
+        self.od_source = None
+
+        self.connect()
+
+    def connect(self):
         self.create_socket()
         self.register()
 
@@ -199,5 +206,19 @@ class HTTPRelay(Relay):
             self.close()
             return
 
-        self.server.add_source(self.path, self.sock, self.address,
-                               self.response_parser, self.burst_size)
+        if self.on_demand and self.od_source:
+            # give back the control to the source
+            self.od_source.on_demand_connected(self.sock, self.response_parser)
+            return
+
+        source = sources.find_source(
+            self.server, self.sock, self.address, self.response_parser,
+            self.path, self.burst_size, self.on_demand)
+        if self.on_demand:
+            self.od_source = source
+        self.server.register_source(source)
+
+    def close(self):
+        Relay.close(self)
+        if self.od_source is not None:
+            self.od_source.close()

savate/shoutcast_source.py
@@ -12,10 +12,16 @@ class ShoutcastSource(LowBitrateSource):
                    'notice2')
 
     def __init__(self, server, sock, address, content_type,
-                 request_parser, path = None, burst_size = None):
+                 request_parser, path = None, burst_size = None,
+                 on_demand=False):
         LowBitrateSource.__init__(self, server, sock, address, content_type,
-                                   request_parser, path, burst_size)
+                                   request_parser, path, burst_size, on_demand)
 
+        self.set_headers()
+
+        self.frame_parser = None
+
+    def set_headers(self):
         # set icy metadata
         for head in self.ICY_HEADERS:
             setattr(self, 'icy_%s' % head,
@@ -28,7 +34,15 @@ class ShoutcastSource(LowBitrateSource):
             self.buffer_metadata = b''
             self.metadata = b''
 
-        self.frame_parser = None
+    def on_demand_desactivate(self):
+        LowBitrateSource.on_demand_desactivate(self)
+        if self.frame_parser is not None:
+            self.frame_parser.clear()
+
+    def on_demand_connected(self, sock, request_parser):
+        # update? headers
+        LowBitrateSource.on_demand_connected(self, sock, request_parser)
+        self.set_headers()
 
     def metadata_parse(self):
         packet_cuts = []
@@ -78,9 +92,9 @@ class ShoutcastSource(LowBitrateSource):
 class MP3ShoutcastSource(ShoutcastSource):
     """Shoutcast Source with MP3 frames parsing support."""
     def __init__(self, server, sock, address, content_type, request_parser,
-                 path = None, burst_size = None):
+                 path = None, burst_size = None, on_demand=False):
         ShoutcastSource.__init__(self, server, sock, address, content_type,
-                                 request_parser, path, burst_size)
+                                 request_parser, path, burst_size, on_demand)
 
         self.frame_parser = MP3Parser()
 
@@ -88,8 +102,8 @@ class MP3ShoutcastSource(ShoutcastSource):
 class ADTSShoutcastSource(ShoutcastSource):
     """Shoutcast Source with ADTS frames parsing support."""
     def __init__(self, server, sock, address, content_type, request_parser,
-                 path = None, burst_size = None):
+                 path = None, burst_size = None, on_demand=False):
         ShoutcastSource.__init__(self, server, sock, address, content_type,
-                                 request_parser, path, burst_size)
+                                 request_parser, path, burst_size, on_demand)
 
         self.frame_parser = ADTSParser()

commit a7edbd7938709b571dfcd3a79ff12a82087d07d5

Fixed PUT sources.
Was broken by eb739984e40769a2e40bc7c65fec51dd5446ae37.

savate/server.py
@@ -117,7 +117,7 @@ class HTTPRequest(looping.BaseIOEventHandler):
 
         if self.request_parser.request_method in [b'PUT', b'SOURCE', b'POST']:
             self.server.register_source(sources.find_source(
-                path, self.sock, self.address, self.request_parser))
+                self.server, self.sock, self.address, self.request_parser, path))
         elif self.request_parser.request_method in [b'GET']:
             # New client
 

savate/sources.py
@@ -32,7 +32,7 @@ class StreamSource(looping.BaseIOEventHandler):
         self.keepalive = keepalive
 
         self.on_demand = self.RUNNING if on_demand else self.DISABLED
-        self.relay = server.relays[sock]
+        self.relay = server.relays.get(sock)  # some sources doesn't have relay
 
     def on_demand_activate(self):
         """Method which reconnects the relay"""